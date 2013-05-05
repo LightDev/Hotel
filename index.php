f36da74b4faec526cc9e9e7b18b47c1ca4d599df
Paweł Parafiniuk <8ightsoft@gmail.com>
2013-05-06 00:22:01 +0200

    Strona glowna ver 0.4

---
+++ index.php
@@ -0,0 +1,229 @@
+<!doctype html>
+<html xmlns="http://www.w3.org/1999/xhtml"><head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+            <title>Hotel & Restaurant</title>
+            <link rel="stylesheet" href="http://code.jquery.com/ui/1.10.2/themes/smoothness/jquery-ui.css" />
+            <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+            <script src="http://code.jquery.com/ui/1.10.2/jquery-ui.js"></script>
+            <script src="js/jsHelper.js"></script>
+            <!--<link rel="stylesheet" href="css/StyleSheet1.css" />-->
+            <script>
+                function logout() {
+                    $.get("logout.php");
+                    return false;
+                }
+            </script>
+            <style type="text/css">
+                @import url('css/StyleSheet1.css');
+            </style>
+    </head>
+
+    <body>
+        <?php
+        $uzytkownicy = array(1 =>
+            array('login' => 'u1', 'haslo' => sha1('pp')),
+            array('login' => 'user2', 'haslo' => sha1('ddd')),
+            array('login' => 'user3', 'haslo' => sha1('fff'))
+        );
+
+        function czyIstnieje($login, $haslo) {
+            global $uzytkownicy;
+            $haslo = sha1($haslo);
+            foreach ($uzytkownicy as $id => $dane) {
+                if ($dane['login'] == $login && $dane['haslo'] == $haslo) {
+                    // O, jest ktos taki - zwroc jego ID
+                    return $id;
+                }
+            }
+            // Jeżeli doszedłeś a tutaj, to takiego użytkownika nie ma
+            return false;
+        }
+
+// end czyIstnieje();
+        session_start();
+        if (!isset($_SESSION['uzytkownik'])) {
+            // Sesja się zaczyna, wiec inicjujemy użytkownika anonimowego
+            $_SESSION['uzytkownik'] = 0;
+        }
+        ?>
+        <div id="header">
+            <div class="wrap">
+                <!--                <div class="logo">
+                                    <a href="#">Logo Of Your Site!</a>
+                                </div>-->
+
+                <?php
+                if ($_SESSION['uzytkownik'] > 0) {
+                    // Ktos jest zalogowany
+                    echo '<a href="#">Moje rezerwacje</a>
+                        <div id="loginButtons" style="width:150px;">Witaj ' . $_SESSION['login'] .
+                    ' <a href = "index.php" onclick="logout();" class = "button gradient_silver">Wyloguj</a>';
+                    ///<a href="#" onclick="doSomething();">Click Me!</a>';
+                } else {
+                    // Niezalogowany
+                    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+                        if (($id = czyIstnieje($_POST['login'], $_POST['haslo'])) !== false) {
+                            // Logujemy uzytkownika, wpisal poprawne dane
+                            $_SESSION['uzytkownik'] = $id;
+                            $_SESSION['uzytkownik'] = $_POST['login'];
+                            echo 'Witaj ' . $_POST['login'] . ' <a href = "#" class = "button gradient_silver">Wyloguj</a>';
+                        } else {
+                            echo 'Podałeś nieprawidłowe login lub hasło';
+                        }
+                    } else {
+                        ?>
+                        <div id="loginButtons" >
+                            <form action = "#" method="POST">
+                                Nr klienta: <input name="login" type="text" /> Hasło: <input name="haslo" type="password" />
+                                <!--<a href = "#" class = "button gradient_gold">Zaloguj się</a>-->
+                                <input type = "submit" class = "button gradient_gold" value = "Zaloguj się" />
+                                <a href = "#" class = "button gradient_silver">Rejestracja</a>
+                            </form>
+                            <?php
+                        }
+                    }
+                    ?>
+                </div>
+                <!--<p>This site is amazing!</p>-->
+            </div>
+        </div>
+        <div id="navigation">
+            <div class="wrap">
+                <ul>
+                           <!--<img src="img/logo.png" style="color:#D5AA65;"/>-->
+                    <li><a href="#" style="margin-top: 10px;color:#D5AA65;font-weight: bold;font-size: 23px;">Hotel & Restaurant</a></li>
+                    <li><a href="#" >POKOJE</a></li>
+                    <li><a href="#" >RESTAURACJA</a></li>
+                    <li><a href="#" >KONFERENCJE</a></li>
+                    <li><a href="#" >OFERTA SPECJALNA</a></li>
+                    <li><a href="#" >PROMOCJE</a></li>
+                </ul>
+            </div>
+        </div>
+
+
+        <div class="wrap">
+            <div id="content" >
+                <form action="reservation_results.php">
+                    <div id="reservationPanel" >
+                        <div class="panel " style="width: 200px;" >
+                            <h2 class="underline">Wybierz liczbę pokoi</h2>
+                            <p >
+                                <select class="aligncenter" name="jezyk" size="1" >
+                                    <option>1 pokój</option>
+                                    <option>2 pokoje</option>
+                                    <option>3 pokoje</option>
+                                    <option>4 pokoje</option>
+                                </select>
+                            </p>
+                            <p class="circle" >1</p>
+                            <!--<div class="circle">1</div>-->
+                        </div>
+                        <div class="panel" style="width: 400px;">
+                            <h2 class="underline">Podaj czas pobytu</h2>
+                            <p>Od:&nbsp;<input type="text" id="dateFrom" class="date_picker" />&nbsp;&nbsp;&nbsp;Do:&nbsp; <input type="text" id="dateTo" class="date_picker" /></p>
+                            <p class="circle">2</p>
+                        </div>
+
+                        <div 
+                            class="panel" 
+                            style="width: 170px;padding-top: 20px;line-height: 10px;">
+                            <p>
+                                <!--<a href="#" class="button buttonSpecialPadding gradient_gold">Sprawdź dostępność</a>-->
+                                <input type="submit" class="button buttonSpecialPadding gradient_gold" value="Sprawdź dostępność">
+                            </p>
+                            <p class="circle">3</p>
+                        </div>
+                </form>
+            </div>
+            <div id="otherOptionsPanel" >
+                <span class="bold">Inne opcje:</span>
+                <a href="#" >Wyszukiwanie zaawansowane</a>
+                <a href="#" id="cancelReservationLink" >Anuluj rezerwację</a>
+            </div>
+            <div id="cancelReservationPanel" style="background: #666;border-top: none;">
+                <form action="#">
+                    Podaj numer rezerwacji: <input id="reservation_id" type="text" />
+                    <!--<a href="#" class="button gradient_silver">Anuluj</a>-->
+                    <input type="submit" value="Anuluj" class="button gradient_silver" />
+                    <span class="error_text">Nie podałeś numeru rezerwacji</span>
+                </form>
+            </div>
+            <!--<div id="wrapper">-->
+            <div  class="panel">
+                <h2 class="underline">INFORMACJE</h2>
+                <table>
+                    <tr>
+                        <td><p><span class="bold">Anulowanie rezerwacji:</span> </p>
+                        </td>
+                        <td>do 18:00</td>
+                    </tr>
+                    <tr>
+                        <td><span class="bold">Palenie:</span></td>
+                        <td>Dozwolone</td>
+                    </tr>
+                    <tr>
+                        <td><span class="bold">Zwierzęta:</span></td>
+                        <td>Dozwolone</td>
+                    </tr>
+                    <tr>
+                        <td>Depozyt:</td>
+                        <td>100zł (Brak refundacji)</td>
+                    </tr>
+                    <tr>
+                        <td>Opieka nad zwierzęciem:</td>
+                        <td>TAK</td>
+                    </tr>
+                    <tr>
+                        <td><span class="bold">Parking:</span></td>
+                        <td>80 miejsc</td>
+                        <tr>
+                            <td>Normalny:</td>
+                            <td>100zł</td>
+                        </tr>
+                        <tr>
+
+                            <td>Z parkingowym:</td>
+                            <td>120zł</td>
+                        </tr>
+                    </tr>
+                </table>
+
+                <p style="text-align: right">
+                    <a href="#" >Więcej szczegółów</a>
+                </p>
+            </div>
+            <div class="panel">
+                <h2 class="underline">SPRAWDŹ DOJAZD</h2>
+<!--<iframe src="http://www.map-generator.org/c367a213-aac7-4bcc-956d-60d9b2c9fdcf/iframe-map.aspx" scrolling="no" height="200px" width="280px" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>                   <p>sadsadd</p>-->
+                <div id="zumiMap" class="zumi_creator" style="width:280px; height:280px;"></div><script src="http://api.zumi.pl/maps/api" type="text/javascript" ></script><script type="text/javascript">(function() {
+                        var marker, map = new zumi.maps.Map("zumiMap", {"apiKey": "DB85F8EBA7EB0780E0434628AE0AA164"});
+                        map.afterLoad(function() {
+                            document.getElementById("zumiMap").className += " zumi_creator";
+                            marker = map.addMarker({lat: 53.117026, lng: 23.144629}, {letter: "A", type: "main"});
+                            marker.bindPopup('<h2 class="title">Hotel & Restaurant</h2><span class="adress">Bia\u0142ystok</span><span class="adress">Wiejska 45C</span><span class="phone">983-345-234</span><span class="mail"><a href="mailto:"></a></span><p class="description"></p><div class="lastLinks"><a class="directions" target="_blank" href="http://www.zumi.pl/firmy,23.144629:53.117026,1,7,namapie.html">w okolicy</a><span class="separate">|</span><a class="directions" target="_blank" href="http://www.zumi.pl/,23.144629:53.117026,1,23.144629,53.117026,1,12,trasa.html">dojazd</a></div>', {minWidth: 192});
+                            marker.openPopup();
+                            map.setCenter({lng: 23.144629, lat: 53.117026}, 8);
+                        });
+                    })();</script>
+            </div>
+            <div class="panel">
+                <h2 class="underline">AKTUALNA POGODA</h2>
+                <p>20 Białystok</p>
+            </div>
+        </div>
+        </div>
+        <!--</div>-->
+        <!--</div>-->
+
+        <div id="footer">
+            <div class="wrap">
+                <p><span class="bold">Hotel & Restaurant</span> <span class="splitter">&nbsp;|&nbsp;</span>
+                    <a href="#">Najczęściej zadawane pytania</a><span class="splitter">&nbsp;|&nbsp;</span>
+                    <a href="#">Regulamin rezerwacji</a><span class="splitter">&nbsp;|&nbsp;</span>
+                    <a href="#">Kontakt</a></p>
+                <p style="width: 200px;float: right">Webdesign: 8SOFT &copy; 2013</p>
+            </div>
+        </div>
+    </body>
+</html>
---
+++ js/jsHelper.js
@@ -0,0 +1,73 @@
+$(document).ready(function() {
+    $(function() {
+        $.datepicker.setDefaults($.datepicker.regional[ "pl" ]);
+        $("#dateFrom").datepicker({
+            autoSize: true, // automatically resize the input field 
+            minDate: new Date(), // prevent selection of date older than today
+            dateFormat: "d MM yy",
+            closeText: 'Zamknij',
+            prevText: '&#x3c;Poprzedni',
+            nextText: 'Następny&#x3e;',
+            currentText: 'Dziś',
+            monthNames: ['Styczeń', 'Luty', 'Marzec', 'Kwiecień', 'Maj', 'Czerwiec',
+                'Lipiec', 'Sierpień', 'Wrzesień', 'Październik', 'Listopad', 'Grudzień'],
+            monthNamesShort: ['Sty', 'Lu', 'Mar', 'Kw', 'Maj', 'Cze',
+                'Lip', 'Sie', 'Wrz', 'Pa', 'Lis', 'Gru'],
+            dayNames: ['Niedziela', 'Poniedzialek', 'Wtorek', 'Środa', 'Czwartek', 'Piątek', 'Sobota'],
+            //dayNamesShort: ['Nie', 'Pn', 'Wt', 'Śr', 'Czw', 'Pt', 'So'], it doesnt work
+            dayNamesMin: ['Nd', 'Pon', 'Wto', 'Śro', 'Czw', 'Pt', 'Sob'],
+            //beforeShowDay: $.datepicker.noWeekends, // Disable selection of weekends
+            firstDay: 1 // Start with Monday
+                    //showOn: "button",
+                    //buttonImage: "img/DatePickerIcon.gif",
+                    //buttonImageOnly: true
+
+//showOn: 'button',
+// Show a button next to the text-field
+//        altFormat: 'dd-mm-yy', // Date Format used
+//        //constrainInput: true, // prevent letters in the input field
+
+        });
+        var disabledDays = ["5-21-2013"];
+        $("#dateTo").datepicker({
+            autoSize: true, // automatically resize the input field 
+            minDate: new Date(), // prevent selection of date older than today
+            dateFormat: "d MM yy",
+            closeText: 'Zamknij',
+            prevText: '&#x3c;Poprzedni',
+            nextText: 'Następny&#x3e;',
+            currentText: 'Dziś',
+            monthNames: ['Styczeń', 'Luty', 'Marzec', 'Kwiecień', 'Maj', 'Czerwiec',
+                'Lipiec', 'Sierpień', 'Wrzesień', 'Październik', 'Listopad', 'Grudzień'],
+            monthNamesShort: ['Sty', 'Lu', 'Mar', 'Kw', 'Maj', 'Cze',
+                'Lip', 'Sie', 'Wrz', 'Pa', 'Lis', 'Gru'],
+            dayNames: ['Niedziela', 'Poniedzialek', 'Wtorek', 'Środa', 'Czwartek', 'Piątek', 'Sobota'],
+            //dayNamesShort: ['Nie', 'Pn', 'Wt', 'Śr', 'Czw', 'Pt', 'So'], it doesnt work
+            dayNamesMin: ['Nd', 'Pon', 'Wto', 'Śro', 'Czw', 'Pt', 'Sob'],
+            //beforeShowDay: $.datepicker.noWeekends, // Disable selection of weekends
+            //beforeShowDay: disabledDays, // Disable selection of weekends
+            firstDay: 1 // Start with Monday
+//            showOn: "button",
+//            buttonImage: "img/DatePickerIcon.gif",
+//            buttonImageOnly: true
+
+//showOn: 'button',
+// Show a button next to the text-field
+//        altFormat: 'dd-mm-yy', // Date Format used
+//        //constrainInput: true, // prevent letters in the input field
+
+        });
+
+        $("#cancelReservationLink").click(function() {
+            $("#cancelReservationPanel").slideToggle(600);
+            //$("#cancelReservationPanel").fadeIn(600);
+            $('#cancelReservationPanel input').blur(function()
+            {
+                if (!$(this).val()) {
+                    $(".error_text").fadeIn(600);
+                    //$(this).parents('p').addClass('warning');
+                }
+            });
+        });
+    });
+});
