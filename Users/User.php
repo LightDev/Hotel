ef72d2103130356a52761639010b02d0cd7d1b05
Paweł Parafiniuk <8ightsoft@gmail.com>
2013-05-11 18:55:38 +0200

    update bazy sql nie bylo skrotu dla hasla a plaintext

---
+++ Users/User.php
@@ -0,0 +1,17 @@
+<?php
+
+abstract class User {
+
+    public function __construct() {
+        
+    }
+
+    public $id;
+    public $name;
+    public $surname;
+    public $login;
+    public $password;
+
+}
+
+?>
